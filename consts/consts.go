d403a91d889c474d69d694b2a75527b43744db43
Sriisshti <138436340+Sriisshti@users.noreply.github.com>
2024-06-27 14:12:09 +0530

    consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "Srishti"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "Sri"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "SK"
 )
 
 var ID ids.ID
